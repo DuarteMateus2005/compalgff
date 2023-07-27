9c2af41533608411afe4c200e7958039d68ba9b1
Duarte Mateus <duartecmateus@gmail.com>
2023-07-27 14:46:36 +0100

    commit

---
+++ headfirst/structures/people.py
@@ -0,0 +1,10 @@
+people = {}
+
+people["Ford"] = {"Name":"Ford Perfect", "Gender":"Male", "Occupation":"Researcher", "Home Planet":"Betelguese"}
+people["Arthur"] = {"Name":"Arthur Dent", "Gender":"Male", "Occupation":"Sadnwich-Maker", "Home Planet":"Earth"}
+people["Trillian"] = {"Name":"Tricia McMillan", "Gender":"Female", "Occupation":"Mathematician", "Home Planet":"Earth"}
+people["Robot"] = {"Name":"Marvin", "Gender":"Unknown", "Occupation":"Paranoid Android", "Home Planet":"Unknown"}
+
+import pprint
+
+pprint.pprint(people)
