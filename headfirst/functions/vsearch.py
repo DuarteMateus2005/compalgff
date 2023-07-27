dce0b58e42ad05611376c9a300690ee9a3ffc6a3
Duarte Mateus <duartecmateus@gmail.com>
2023-07-27 15:23:22 +0100

    commit

--- headfirst/functions/vsearch.py
+++ headfirst/functions/vsearch.py
@@ -1,5 +1,4 @@
 def search4vowels(word):
     """Returns any vowels found in a suppplied word."""
     vowels = {'a','e','i','o','u'}
-    found = vowels.intersection(set(word))
-    return found
+    return vowels.intersection(set(word))
