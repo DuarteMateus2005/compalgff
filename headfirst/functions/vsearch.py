b831c7f41c0c040397a4792a3b99c675056199de
Duarte Mateus <duartecmateus@gmail.com>
2023-07-27 15:20:54 +0100

    commit

--- headfirst/functions/vsearch.py
+++ headfirst/functions/vsearch.py
@@ -1,6 +1,5 @@
 def search4vowels(word):
-    """Displays any found vowels in an asked-for word."""
+    """Returns a boolean based on any vowels found."""
     vowels = {'a','e','i','o','u'}
     found = vowels.intersection(set(word))
-    for vowel in found:
-        print(vowel)
+    return bool(found)
